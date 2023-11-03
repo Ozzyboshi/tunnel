29d7218421fe85a2ac583d01e18c3c4dddca3ed6
Ozzyboshi <gun101@email.it>
2023-11-03 18:18:21 +0100

    43

--- tunnel.s
+++ tunnel.s
@@ -16,32 +16,26 @@ PRINT_PIXELS MACRO
   move.w            (a4)+,d4
 
   ; add shift x (add frame counter to what was read from the distance table and perform a %16)
-  ;add.w             FRAME_COUNTER,d2
   ; frame counter is on the upper part of d7 to save access memory
   add.w             d3,d2
   and.w             d5,d2
-  ;add.w             d2,d2
 
   ; now d4 holds the correct offset of the table in the lower word
   add.w             d2,d4
 
-  moveq             #$0,d1
-
-  tst.b             0(a2,d4.w) ; check if we have to print color 1 or color 2
-  beq.s             pixel_1_done\1 ; if color 1 has to be printed on screen
-  move.w            d6,d1
-pixel_1_done\1:
+  move.b            0(a6,d4.w),d1 ; check if we have to print color 1 or color 2
+  ext.w             d1
+  and.w             d6,d1
 
   ; pixel 2 start
   move.w            (a3)+,d2
   move.w            (a4)+,d4
   add.w             d3,d2
   and.w             d5,d2
-  ;add.w             d2,d2
   add.w             d2,d4
   tst.b             0(a2,d4.w)
   beq.s             pixel_2_done\1
-  or.w             d0,d1
+  or.w              d0,d1
 pixel_2_done\1:
 
 ; pixel 3 start
@@ -49,12 +43,8 @@ pixel_2_done\1:
   move.w            (a4)+,d4
   add.w             d3,d2
   and.w             d5,d2
-  ;add.w             d2,d2
   add.w             d2,d4
-  tst.b             0(a2,d4.w)
-  beq.s             pixel_3_done\1
-  ori.w             #$00F0,d1
-pixel_3_done\1:
+  or.b              0(a1,d4.w),d1
 
 ; start of pixel 4
   move.w            (a3)+,d2
@@ -74,9 +64,9 @@ pixel_3_done\1:
 
   ; Place addr in d0 and the copperlist pointer addr in a1 before calling
 POINTINCOPPERLIST MACRO
-  move.w              d5,6(a1)
+  move.w              d5,6(a5)
   swap                d5
-  move.w              d5,2(a1)
+  move.w              d5,2(a5)
   ENDM
   jmp Inizio
 
@@ -127,7 +117,7 @@ tunnel_x_prepare:
   ; END preparing bitplane 0, set FF in every byte where the tunnel will be drown
 
   ; Set bpl zero in copperlist
-  lea               BPLPTR1,a1
+  lea               BPLPTR1,a5
   move.l            SCREEN_PTR_0,d5
   POINTINCOPPERLIST
 
@@ -139,7 +129,7 @@ transformation_table_y_loop:
   jsr               GENERATE_Y_TRANSFORMATION_TABLE
   addq              #1,d6
   dbra              d5,transformation_table_y_loop
-  ; END: Prepare Y rotation transformation table 
+  ; END: Prepare Y rotation transformation table
 
   ; Generate transformation table for distance
   jsr               GENERATE_TRANSFORMATION_TABLE
@@ -157,8 +147,8 @@ transformation_table_y_loop:
 
   moveq             #0,d3 ; reset current time variable
   lea	              TRASFORMATION_TABLE_Y_0(PC),a4
-    move.w #$F000,d6
-    move.w #$0F00,d0
+  move.w            #$F000,d6
+  move.w            #$0F00,d0
 
 
 ; ******************************* START OF GAME LOOP ****************************
@@ -177,37 +167,33 @@ Aspetta:
   beq.s             Aspetta
 
   ;bra.w tunnelend  ; skip tunnel rendering
-  
+
   ; Switch Bitplanes for double buffering
   neg.l             SCREEN_OFFSET
   move.l            SCREEN_OFFSET,d5
-  ;move.l            SCREEN_PTR_0,SCREEN_PTR_OTHER_0
   move.l            SCREEN_PTR_1,SCREEN_PTR_OTHER_1
-  ;add.l             d5,SCREEN_PTR_0
   add.l             d5,SCREEN_PTR_1
-  
+
   IFD COLORDEBUG
   move.w #$FF0,$dff180
   ENDC
 
-  
   ;bra.w     tunnelend
 
   ; *********************************** Start of tunnel rendering *********************************
   lea               TEXTURE_DATA(PC),a2
+  lea               TEXTURE_DATA_2(PC),a6
+  lea               TEXTURE_DATA_3(PC),a1
   lea               TRANSFORMATION_TABLE_DISTANCE(PC),a3
 
-
-  ;SETBITPLANE       0,a6
   SETBITPLANE       1,a5
-  ;move.l  SCREEN_PTR_0,a6
   moveq             #$F,d5
 
   ; y cycle start
   IFND TUNNEL_SCANLINES
-  moveq              #SCREEN_RES_Y-1,d7
+  moveq             #SCREEN_RES_Y-1,d7
   ELSE
-  moveq              #TUNNEL_SCANLINES-1,d7
+  moveq             #TUNNEL_SCANLINES-1,d7
   ENDC
 tunnel_y:
 
@@ -215,24 +201,22 @@ tunnel_y:
   ;moveq             #SCREEN_RES_X/4-1,d6
 ;tunnel_x:
 
-  PRINT_PIXELS 1
-  PRINT_PIXELS 2
-  PRINT_PIXELS 3
-  PRINT_PIXELS 4
-  PRINT_PIXELS 5
-  PRINT_PIXELS 6
-  PRINT_PIXELS 7
-  PRINT_PIXELS 8
-  PRINT_PIXELS 9
-  PRINT_PIXELS 10
-  PRINT_PIXELS 11
-  PRINT_PIXELS 12
-  PRINT_PIXELS 13
-  PRINT_PIXELS 14
-  PRINT_PIXELS 15
-  PRINT_PIXELS 16
-
-  ;dbra              d6,tunnel_x
+  PRINT_PIXELS      1
+  PRINT_PIXELS      2
+  PRINT_PIXELS      3
+  PRINT_PIXELS      4
+  PRINT_PIXELS      5
+  PRINT_PIXELS      6
+  PRINT_PIXELS      7
+  PRINT_PIXELS      8
+  PRINT_PIXELS      9
+  PRINT_PIXELS      10
+  PRINT_PIXELS      11
+  PRINT_PIXELS      12
+  PRINT_PIXELS      13
+  PRINT_PIXELS      14
+  PRINT_PIXELS      15
+  PRINT_PIXELS      16
 
   ; change scanline
   lea               8+40*0(a5),a5
@@ -255,13 +239,12 @@ norestorey:
 
   ; load bitplanes in copperlist
 
-  lea               BPLPTR2,a1
+  lea               BPLPTR2,a5
   move.l            SCREEN_PTR_1,d5
   POINTINCOPPERLIST
 
   ; increment the frame counter for animating
-  ;add.w             #1,FRAME_COUNTER
-  addq               #1,d3
+  addq              #1,d3
 
   ; exit if lmb is pressed
   btst              #6,$bfe001
@@ -279,6 +262,8 @@ XOR_TEXTURE:
   ;  texture[y][x] = (x * 256 / texWidth) ^ (y * 256 / texHeight);
   ;}
   lea               TEXTURE_DATA(PC),a2
+  lea               TEXTURE_DATA_2(PC),a3
+  lea               TEXTURE_DATA_3(PC),a4
   clr.w             CURRENT_X
   clr.w             CURRENT_Y
 
@@ -300,11 +285,15 @@ xor_texture_x:
   ; if d7 > 127 color is 1
   IF_1_LESS_EQ_2_W_U #TEXTURE_SIZE/2,d5,.notgreater,s
   ;STROKE #1
+  clr.b             (a4)+
   clr.b             (a2)+
+  clr.b             (a3)+
   bra.s             .printpoint
 .notgreater:
   ;STROKE #2
+  move.b            #$F0,(a4)+
   move.b            #$0F,(a2)+
+  move.b            #$FF,(a3)+
 .printpoint
 	;jsr               POINT
   addi.w            #1,CURRENT_X
@@ -314,22 +303,22 @@ xor_texture_x:
   dbra              d7,xor_texture_y
   rts
 
-  ; Routine GENERATE_Y_TRANSFORMATION_TABLE
-  ; This routine reads table TRASFORMATION_TABLE_Y which is created be the
-  ; "transformationtable.c" and recalculates it using a different shiftY.
-  ; shiftY value must be stored on d6
-  ; the destination table must be stored on a6
-  GENERATE_Y_TRANSFORMATION_TABLE:
-                    lea	              TRASFORMATION_TABLE_Y(PC),a4
-                    move.w            #SCREEN_RES_X*SCREEN_RES_Y-1,d7
-  generate_y_transformation_table_loop:
-                    move.w           (a4)+,d0
-                    add.w            d6,d0
-                    andi.w           #$F,d0
-                    muls.w           #16,d0
-                    move.w           d0,(a6)+
-                    dbra             d7,generate_y_transformation_table_loop
-                    rts
+; Routine GENERATE_Y_TRANSFORMATION_TABLE
+; This routine reads table TRASFORMATION_TABLE_Y which is created be the
+; "transformationtable.c" and recalculates it using a different shiftY.
+; shiftY value must be stored on d6
+; the destination table must be stored on a6
+GENERATE_Y_TRANSFORMATION_TABLE:
+  lea	              TRASFORMATION_TABLE_Y(PC),a4
+  move.w            #SCREEN_RES_X*SCREEN_RES_Y-1,d7
+generate_y_transformation_table_loop:
+  move.w            (a4)+,d0
+  add.w             d6,d0
+  andi.w            #$F,d0
+  muls.w            #16,d0
+  move.w            d0,(a6)+
+  dbra              d7,generate_y_transformation_table_loop
+  rts
 
 ; Routine GENERATE_TRANSFORMATION_TABLE
 ; This routine generates the precalculated table used for the X axis
@@ -347,7 +336,7 @@ xor_texture_x:
 ;   }
 ; }
 ; Resulting table will be stored att addr TRANSFORMATION_TABLE_DISTANCE
-; 
+;
 GENERATE_TRANSFORMATION_TABLE:
   lea               TRANSFORMATION_TABLE_DISTANCE(PC),a0
 
@@ -467,7 +456,10 @@ TRANSFORMATION_TABLE_DISTANCE:
 
 TEXTURE_DATA:
   dcb.w TEXTURE_HEIGHT*TEXTURE_HEIGHT,0
-
+TEXTURE_DATA_2:
+  dcb.w TEXTURE_HEIGHT*TEXTURE_HEIGHT,0
+TEXTURE_DATA_3:
+  dcb.w TEXTURE_HEIGHT*TEXTURE_HEIGHT,0
 ;---------------------------------------------------------------
 Saveint:              dc.w 0
 SaveDMA:              dc.w 0
@@ -480,12 +472,7 @@ TRASFORMATION_TABLE_Y:
 TRASFORMATION_TABLE_Y_0: dcb.w SCREEN_RES_X*SCREEN_RES_Y*64,0
 TRASFORMATION_TABLE_Y_0_END:
 
-	include "AProcessing/libs/matrix/matrixcommon.s"
-	include "AProcessing/libs/matrix/matrix.s"
-	include "AProcessing/libs/matrix/point.s"
-	include "AProcessing/libs/rasterizers/point.s"
 	include "AProcessing/libs/rasterizers/processing_bitplanes_fast.s"
-	include "AProcessing/libs/rasterizers/processing_table_plotrefs.s"
 
 ;----------------------------------------------------------------
 
@@ -850,8 +837,29 @@ BPLPTR2:
   ;dc.w       $180,0
   dc.w       $10a,0
 
+                 ; line 41
+  dc.w       $A1E3,$FFFE
+  ;dc.w       $180,$fff
+  dc.w       $10a,-40
+  dc.w       $A3E3,$FFFE
+  ;dc.w       $180,0
+  dc.w       $10a,0
 
+                  ; line 42
+  dc.w       $A4E3,$FFFE
+  ;dc.w       $180,$fff
+  dc.w       $10a,-40
+  dc.w       $A6E3,$FFFE
+  ;dc.w       $180,0
+  dc.w       $10a,0
 
+                    ; line 43
+  dc.w       $A7E3,$FFFE
+  ;dc.w       $180,$fff
+  dc.w       $10a,-40
+  dc.w       $A9E3,$FFFE
+  ;dc.w       $180,0
+  dc.w       $10a,0
 
   ; Copperlist end
   dc.w       $FFFF,$FFFE                                               ; End of copperlist
