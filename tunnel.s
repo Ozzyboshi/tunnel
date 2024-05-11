987f10c26b3cda618763185863437ebfce4613a9
Ozzyboshi <gun101@email.it>
2024-05-11 12:02:01 +0200

    further cleanup

--- tunnel.s
+++ tunnel.s
@@ -143,17 +143,6 @@ TEXTURE_LIST: dc.l CHECKERS
               dc.l SQUARE
               dc.l 0
 
-ANGTABLE:
-    dc.w %0010110100000000 ; 45
-    dc.w %0001101010010000 ; 26.565
-    dc.w %0000111000001001 ; 14.036
-    dc.w %0000011100100000 ; 7.125
-    dc.w %0000001110010011 ; 3.576
-    dc.w %0000000111001010 ; 1.790
-    dc.w %0000000011100101 ; 0.895
-    dc.w %0000000001110010 ; 0.448
-    dc.w 0
-
 COLORSTUNNEL:
     dc.w $0F,$00,$00,$00,$00,$00 ; from black to red
     dc.w $00,$00,$0F,$00,$00,$07 ; from green to dark blue
@@ -422,7 +411,7 @@ tunnel_x_prepare:
   LEA               SpritePointers,a5
   POINTINCOPPERLIST
 
-  ; Sprite 1 init
+  ; Sprite 00 init
   MOVE.L            #MYSPRITE00,d5
   LEA               SpritePointers+8,a5
   POINTINCOPPERLIST
@@ -432,7 +421,7 @@ tunnel_x_prepare:
   LEA               SpritePointers+16,a5
   POINTINCOPPERLIST
 
-  ; Sprite 2 init
+  ; Sprite 11 init
   MOVE.L            #MYSPRITE01,d5
   LEA               SpritePointers+24,a5
   POINTINCOPPERLIST
@@ -459,9 +448,9 @@ xloop:
 
   move.w            #$FF,16*2*2(a0)
   move.w            d7,16*3*2(a0)
-  neg.w d7
+  neg.w             d7
   move.w            d7,16*4*2(a0)
-  neg.w d7
+  neg.w             d7
 
   move.w            d0,16*2(a0)
   not.w             d0
@@ -1120,67 +1109,7 @@ noerrore:
   dbra d6,test9loopx
   rts
 
-CORDIC:
-    ; Load angle table into a0
-    lea ANGTABLE,a0
-
-    ; init Shiftcounter , register is d6
-    moveq #0,d6
-
-    ; init SumAngle , register is d3
-    moveq #0,d3
-
-cordicloop:
-    ; check if Y is positive
-    tst.w d1
-    bmi cordicnegative
-
-    ; ********************* Y is positive ***********************
-    ; Xnew = X + Y >> Shiftcounter
-    move.w d1,d5 ; first we have to shift Y , use scratch register d5
-    asr.w d6,d5  ; Y is now shifted into d5
-    add.w d0,d5  ; Now d5 holds Xnew
-
-    ; Ynew = Y - X >> Shiftcounter
-    move.w d0,d4 ; first we have to shift Y , use scratch register d4
-    neg.w d4
-    asr.w d6,d4  ; Y is now shifted into d5
-    add.w d1,d4
-
-    add.w (a0)+,d3 ; SumAngle += AngTable[i]
-
-    bra cordicincreaseangle
-
-
-cordicnegative:
-    ; ********************* Y is NEGATIVE ***********************
-    ; Xnew = X + Y >> Shiftcounter
-    move.w d1,d5 ; first we have to shift Y , use scratch register d5
-    asr.w d6,d5  ; Y is now shifted into d5
-    neg d5
-    add.w d0,d5  ; Now d5 holds Xnew
-
-    ; Ynew = Y - X >> Shiftcounter
-    move.w d0,d4 ; first we have to shift Y , use scratch register d4
-    asr.w d6,d4  ; Y is now shifted into d5
-    add.w d1,d4
-
-    sub.w (a0)+,d3 ; SumAngle -= AngTable[i]
-
-cordicincreaseangle:
-
-    move.w d5,d0
-    move.w d4,d1
-    beq CORDINCEND
-    tst.w (a0)
-    beq CORDINCEND
-
-    addq #1,d6 ; Increment shifting
-
-    ; cycle over
-    bra cordicloop
-CORDINCEND:
-    rts
+  include "AProcessing/libs/vectors/cordic.s"
 
 Restore_all:
   move.l            SaveIRQ,$6c
@@ -1323,4 +1252,3 @@ SPACESHIP:
   include "sprites/spaceship_back_left.s"
   include "sprites/spaceship_back_right.s"
   end
-
